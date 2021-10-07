5e8ea3b7abf74f0b42cc7db05e10f27676191e85
Daniel Cu Sanchez <danielcu.sanchez@gmail.com>
2021-10-07 13:41:06 -0500

    Rules y functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-const { appFunctions } = require("./utils/appFunctions")
-
+const { appFunctions } = require('./utils/appFunctions')
 
 const app = () => {
-  const file = appFunctions.openFile("test1.txt")
+  const file = appFunctions.openFile('test1.txt')
   const data = appFunctions.getInitialData(file)
-  console.log(data)
-  const transitionTable = appFunctions.getTransitionTable(data)
-  console.log(transitionTable)
+  // console.log(data)
+  // const transitionTable = appFunctions.getTransitionTable(data)
+  // console.log(transitionTable)
+  const result = appFunctions.getExtendedTransitionFunction(data, 'q0', 'adb')
+  if (result[0] === undefined) {
+    console.log('La cadena no es parte del automata')
+  } else {
+    console.log(result)
+  }
 }
-
-
 
 app()
--- src/utils/appFunctions.js
+++ src/utils/appFunctions.js
@@ -1,15 +1,15 @@
-const fs = require("fs")
-const path = require("path")
-
+/* eslint-disable array-callback-return */
+const fs = require('fs')
+const path = require('path')
 
 exports.appFunctions = {
-  //Abre el archivo
+  // Abre el archivo
   openFile: (file) => {
-    const fileOpened = fs.readFileSync(path.join(__dirname, `../test/${file}`), { encoding: "utf-8" })
-    const fileFormated = fileOpened.split("\r").map(line => line.replace("\n", ""))
+    const fileOpened = fs.readFileSync(path.join(__dirname, `../test/${file}`), { encoding: 'utf-8' })
+    const fileFormated = fileOpened.split('\r').map(line => line.replace('\n', ''))
     return fileFormated
   },
-  //Toma los datos iniciales
+  // Toma los datos iniciales
   getInitialData: (file) => {
     const states = accessToFile(file, 0)
     const keys = accessToFile(file, 1)
@@ -48,11 +48,32 @@ exports.appFunctions = {
       })
     })
     return transitionTable
+  },
+  getTransitionFunction: (data, state, char) => {
+    const transitionTable = this.appFunctions.getTransitionTable(data)
+    return transitionTable[state][char]
+  },
+  getExtendedTransitionFunction: (data, state, chain) => {
+    if (chain.length === 0) {
+      return [state]
+    } else if (chain.length === 1) {
+      return this.appFunctions.getTransitionFunction(data, state, chain)
+    } else {
+      const u = chain[0]
+      const a = chain[chain.length - 1]
+      const aux = this.appFunctions.getExtendedTransitionFunction(data, state, u) // llamada recursiva
+      const union = aux.map(state => {
+        const aux2 = this.appFunctions.getTransitionFunction(data, state, a)
+        return aux2
+      })
+      return union
+    }
   }
 }
+// Funciones auxiliares
 const accessToFile = (file, line) => file[line].split(',')
 const cutFile = (file, start) => file.splice(start, file.length)
 const formatTransition = (transition) => {
-  let tf = transition.split(',')
+  const tf = transition.split(',')
   return tf
 }
